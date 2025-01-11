e62b68b63af7840dc97a5207fac22de34a6f658d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-01-11 19:27:51 +0900

    Fix: Fix level data

--- shared/src/level/data/index.ts
+++ shared/src/level/data/index.ts
@@ -1,11 +1,11 @@
-// import { LevelData } from '@sonolus/core'
+import { LevelData } from '@sonolus/core'
 // import { promises as fs } from 'fs'
 // // import { chsToUSC } from '~lib/src/chs/convert.cjs'
 // // import { susToUSC } from '~lib/src/sus/convert.cjs'
 // // import { mmwsToUSC } from '~lib/src/mmws/convert.cjs'
 // import { anyToUSC } from 'usctool'
 // import { uscToLevelData } from '~lib/src/convert.cjs'
-// // import expert from './test.json'
+import expert from './expert.json'
 
 // export const data: LevelData = await fs
 //     .readFile('./shared/src/level/data/ongeki.sus')
@@ -13,9 +13,9 @@
 //     .then(({ usc }) => uscToLevelData(usc))
 
 // await fs.writeFile('./shared/src/level/data/test.levelData.json', JSON.stringify(data, null, 4))
-// export const data: LevelData = expert
+export const data: LevelData = expert
 //
-import type { LevelData } from "@sonolus/core";
-import level from "./test.levelData.json";
-
-export const data: LevelData = level;
+// import type { LevelData } from "@sonolus/core";
+// import level from "./test.levelData.json";
+//
+// export const data: LevelData = level;
