9d05c22944e597ad1fdd0eb9fbd5af4f193634b8
Gaven <167988752+gaven1880@users.noreply.github.com>
2025-03-30 22:18:59 +0400

    Update CriticalTraceNote.ts

--- play/src/engine/playData/archetypes/notes/flatNotes/traceNotes/CriticalTraceNote.ts
+++ play/src/engine/playData/archetypes/notes/flatNotes/traceNotes/CriticalTraceNote.ts
@@ -25,8 +25,11 @@ export class CriticalTraceNote extends TraceNote {
 
   effects = {
     circular: particle.effects.criticalTraceNoteCircular,
+    circularFallback: particle.effects.criticalSlideTickNote,
     linear: particle.effects.criticalTraceNoteLinear,
+    linearFallback: particle.effects.criticalNoteLinear,
   };
+
 
   windows = windows.tapNote.critical;
 
